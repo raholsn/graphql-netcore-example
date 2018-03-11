e9dd95e97defc44eb40044c65527846076c0a35c
Rasmus Olsson <raol@L103066>
2018-03-11 13:40:58 +0100

    added query argument

--- GraphQl.WebAPI/GraphQl/Queries/CustomerQuery.cs
+++ GraphQl.WebAPI/GraphQl/Queries/CustomerQuery.cs
@@ -1,4 +1,6 @@
-﻿using GraphQl.WebAPI.GraphQl.Models;
+﻿using System.ComponentModel;
+
+using GraphQl.WebAPI.GraphQl.Models;
 using GraphQl.WebAPI.GraphQl.Types;
 
 using GraphQL.Types;
@@ -9,7 +11,39 @@ namespace GraphQl.WebAPI.GraphQl.Queries
     {
         public CustomerQuery()
         {
-            Field<CustomerType>("customer", resolve: context => new Customer { Id = 1, Name = "R2-D2" });
+            Field<CustomerType>("customer",
+                arguments: new QueryArguments
+                {
+                    new QueryArgument<IntGraphType>{ Name = "id", Description = "the customers identifier"  },
+                    new QueryArgument<StringGraphType>{Name = "name", Description = "the name of the customer"}
+                },
+                resolve: GetCustomer);
+        }
+
+        private Customer GetCustomer(ResolveFieldContext<object> context)
+        {
+            var id = context.GetArgument<int>("id");
+
+            if (id != default(int))
+            {
+                return new Customer
+                {
+                    Name = "hej",
+                    Id = id
+                };
+            }
+
+            var name = context.GetArgument<string>("name");
+            if (name != null)
+            {
+                return new Customer
+                {
+                    Name = name,
+                    Id = 1
+                };
+            }
+
+            throw new System.NotImplementedException();
         }
     }
 }
